141c9af414d742a01462c328097475996f537888
natashabhuta <natasha.bhuta@gmail.com>
2018-08-15 02:51:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School 
+attr_accessor :name, :roster
+
+  def initialize(name)
+   @name = name
+   @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+   #needs to add a name to the roster array 
+   @student_name = name
+   @grade = grade
+   
+   @roster[grade] ||= []
+   @roster[grade] << "#{student_name}"
+   
+   
+ end
+    
+  def grade(grade)
+    @roster[grade]
+  end 
+
+
+def sort(student_name)
+  @roster.sort_by{|names| name}
+end 
+end 
